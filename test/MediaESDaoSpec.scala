8f85a615572f5d082c748bc320bed3ba151130a8
Hyunsung Jo <Jo@Hyunsungs-iMac.local>
2015-01-14 00:28:04 +0900

    add search test to MediaESDaoSpec

--- test/MediaESDaoSpec.scala
+++ test/MediaESDaoSpec.scala
@@ -6,19 +6,14 @@ import scala.concurrent.duration._
 class MediaESDaoSpec extends Specification {
   section("Media")
   "MediaESDaoSpec" should {
-    "putMedia and getMedia" in {
+    "be able to putMedia, getMedia and searchMedia" in {
       val timeout = 5000L
-      val id = 0
-      // put
+      val id = 11
       val media = Media(id, 1, Seq(1), 1, "http://stuffpoint.com/kate-upton/image/298407-kate-upton-kate-upton-2.jpg", "picture of Kate Upton", Seq("melons"), 1, 0, 1418565692, 1418565692, 1024, 1024, 1)
-      val f1 = MediaESDao.putMedia(media)
-      val putMediaResult = Await.result(f1, timeout milli)
 
-      // get
-      val f2 = MediaESDao.getMedia(id)
-      val getMediaResult = Await.result(f2, timeout milli)
-
-      getMediaResult.getId must equalTo(id.toString)
+      Await.result(MediaESDao.putMedia(media), timeout milli).isCreated must equalTo(true) // put
+      Await.result(MediaESDao.getMedia(id), timeout milli).getId must equalTo(id.toString) // get
+      Await.result(MediaESDao.searchMedia("Kate Upton"), timeout milli).getSuccessfulShards must not equalTo(0) // search
     }
   }
 }
